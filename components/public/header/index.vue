c2fbc284c83b1b4107977bc5e69f298338250abd
linph <370345882@qq.com>
2018-10-26 17:27:35 +0800

    '

--- components/public/header/index.vue
+++ components/public/header/index.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="m-header">
     <el-row 
-      :gutter="15" 
+      :gutter="0" 
       class="m-header">
-      <el-col :span="6">
+      <el-col :span="4">
         <geo />
       </el-col>
       <el-col :span="6">
         <user />
       </el-col>
-      <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="14">
+        <nav />
+      </el-col>
     </el-row>
   </div>
 </template>
@@ -17,10 +19,12 @@
 <script>
 import Geo from './geo'
 import User from './user'
+import Nav from './nav'
 export default {
   components: {
     Geo,
-    User
+    User,
+    Nav
   }
 }
 </script>
---
+++ components/public/header/nav.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="m-nav">
+    123
+    <ul class="nav">
+      <li class="list">
+        <nuxt-link to="/my">我的美团</nuxt-link>
+        <dl>
+          <dd><nuxt-link to="order">我的订单</nuxt-link></dd>
+          <dd><nuxt-link to="order">我的收藏</nuxt-link></dd>
+          <dd><nuxt-link to="order">抵用券</nuxt-link></dd>
+          <dd><nuxt-link to="order">账户设置</nuxt-link></dd>
+        </dl>
+      </li>
+      <li class="list bd">
+        <nuxt-link to="/orfer">手机APP</nuxt-link>
+      </li>
+      <li class="list bd">
+        <nuxt-link to="center">商家中心</nuxt-link>
+      </li>
+      <li class="list site">
+        <nuxt-link to="site">网站导航</nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
