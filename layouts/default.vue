60406149ffdf94eaee6ec6950b71aeef30c7b033
linph <370345882@qq.com>
2018-10-26 15:49:34 +0800

    init project

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-container>
+    <el-header class="layout-default">
+      <home-header />
+    </el-header>
+    <el-main>
+      <nuxt />
+    </el-main>
+    <el-footer>footer</el-footer>
+  </el-container>
+</template>
+
+<script>
+import HomeHeader from '@/components/public/header/index'
+export default {
+  components: {
+    HomeHeader
+  }
+}
+</script>
+
+<style>
+</style>
