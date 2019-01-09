19c0b8923762c190573af2d17a24a3a6d32e9fd5
linxiaohui <370345882@qq.com>
2019-01-09 22:34:56 +0800

    添加登出页面

---
+++ pages/exit.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class=""></div>
+</template>
+
+<script>
+  export default {
+    layout: 'blank',
+    middlewart: async (ctx) => {
+      let { status, data } = await ctx.$axios.post('/users/exit')
+      if (status === 200 && data && data.code == 0) {
+        window.location.href = '/'
+      }
+    }
+  }
+</script>
