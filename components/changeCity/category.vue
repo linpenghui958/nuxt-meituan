b73467e61b24960d34a3bd170b3820a2ace879e2
linxiaohui <370345882@qq.com>
2019-01-15 18:08:33 +0800

    添加category

--- components/changeCity/category.vue
+++ components/changeCity/category.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>category</div>
+  <div>
+    <dl class="m-category">
+      <dt>按拼音首字母选择：</dt>
+      <dd
+        v-for="item in list"
+        :key="item">
+        <a :href="'#city-' + item">{{ item }}</a>
+      </dd>
+    </dl>
+    <dl
+      v-for="item in block"
+      :key="item.title"
+      class="m-category-section">
+      <dt :id="'city-' + item.title"> {{ item.title }}</dt>
+      <dd>
+        <span
+          :v-for="c in item.city"
+          :key="c">{{ c }}</span>
+      </dd>
+    </dl>
+  </div>
 </template>
 
 <script>
